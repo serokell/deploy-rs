58a934b997bb06f475bf707d0994f1fa37584cc1
notgne2 <gen2@gen2.space>
2020-10-05 19:53:59 -0700

    Clean up `simple` example

--- examples/simple/flake.nix
+++ examples/simple/flake.nix
@@ -7,34 +7,15 @@
 
   inputs.deploy-rs.url = "github:serokell/deploy-rs";
 
-  outputs = { self, nixpkgs, deploy-rs }:
-    let
-      setActivate = base: activate: nixpkgs.legacyPackages.x86_64-linux.symlinkJoin {
-        name = ("activatable-" + base.name);
-        paths = [
-          base
-          (nixpkgs.legacyPackages.x86_64-linux.writeTextFile {
-            name = base.name + "-activate-path";
-            text = ''
-              #!${nixpkgs.legacyPackages.x86_64-linux.runtimeShell}
-              ${activate}
-            '';
-            executable = true;
-            destination = "/activate";
-          })
-        ];
+  outputs = { self, nixpkgs, deploy-rs }: {
+    deploy.nodes.example = {
+      hostname = "localhost";
+      profiles.hello = {
+        user = "balsoft";
+        path = deploy-rs.lib.x86_64-linux.setActivate nixpkgs.legacyPackages.x86_64-linux.hello "./bin/hello";
       };
-    in
-    {
-
-      deploy.nodes.example = {
-        hostname = "localhost";
-        profiles.hello = {
-          user = "balsoft";
-          path = deploy-rs.lib.x86_64-linux.setActivate nixpkgs.legacyPackages.x86_64-linux.hello "./bin/hello";
-        };
-      };
-
-      checks = { "x86_64-linux" = { jsonSchema = deploy-rs.lib.x86_64-linux.checkSchema self.deploy; }; };
     };
+
+    checks = { "x86_64-linux" = { jsonSchema = deploy-rs.lib.x86_64-linux.checkSchema self.deploy; }; };
+  };
 }
