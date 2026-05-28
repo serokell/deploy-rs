c48eddae172f9fe39674faa6fe43196e74204643
Andrey Albershteyn <aalbersh@kernel.org>
2026-05-28 23:28:53 +0200

    Update flake.lock and fix 403 on crate.io
    nixos-unstable has a work-around for crate.io returning 403 on the
    requests without user-agent. The Nix's rust package fetcher
    fetchCargoVendor uses python's requests library which doesn't set agent
    explicitly. From recently create.io returns 403 on such requests
    (upstream issues says that nixpkgs has exception but peoples seems to
    get 403 anyway). cargoLock uses uses fetchurl default curl user-agent
    which gets blocked by crate.io

--- flake.nix
+++ flake.nix
@@ -33,7 +33,9 @@
             ".*\.rs$"
           ];
 
-          cargoLock.lockFile = ./Cargo.lock;
+          cargoDeps = final.rustPlatform.importCargoLock {
+            lockFile = ./Cargo.lock;
+          };
 
           meta = {
             description = "A Simple multi-profile Nix-flake deploy tool"; 
